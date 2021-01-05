ace728a9310542d59a2444a46ff91bb47a928e68
KwakSuin <mygirls0@naver.com>
2021-01-05 19:38:36 +0900

    Weather Application

---
+++ app/src/main/java/kwaksuin/portfolio/weatherapp/AsyncTask.java
@@ -0,0 +1,124 @@
+package kwaksuin.portfolio.weatherapp;
+
+import android.os.AsyncTask;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+
+// UI 작업을 위해 별도의 스레드로 만들어져서 백그라운드에서 실행되는 태스크
+public class AsyncTask1 extends AsyncTask<Integer, Integer, Void> {
+    static String result = "";
+    String line;
+    String nowday;
+    String nowtime;
+
+    @Override
+    protected Void doInBackground(Integer... integers) {
+        //파싱
+
+        try {
+            parstring.miseparsing();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        parstring.chodanparsing();
+        parstring.dongnae2parsing();
+        parstring.dongnae5parsing();
+        parstring.haedal();
+        parstring.midterm();
+
+
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+
+    //오늘 날짜 가져오기 (api 변수에 사용합니다.)
+    public static String doYearMonthDay() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd", Locale.KOREA);
+        Date date = new Date();
+        String currentDate = formatter.format(date);
+        return currentDate;
+    }
+
+    //현재 시간 가져오기 (api 변수에 사용합니다.)
+    public static String doTime() {
+        SimpleDateFormat formatter = new SimpleDateFormat("HH00", Locale.KOREA);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.HOUR_OF_DAY,-1);
+
+        String currentDate = formatter.format(calendar.getTime());
+
+        return currentDate;
+    }
+
+    public static String doyes(){
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd", Locale.KOREA);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE,-1);  // 오늘 날짜에서 하루를 뺌.
+        String date = formatter.format(calendar.getTime());
+        return date;
+    }
+
+    public static String today_1(){
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd", Locale.KOREA);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, +1);  // 내일.
+        String date = formatter.format(calendar.getTime());
+        return date;
+    }
+
+    public static String today_2(){
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd", Locale.KOREA);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, +2);  //
+        String date = formatter.format(calendar.getTime());
+        return date;
+    }
+
+    public static String today_3(){
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd", Locale.KOREA);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, +3);
+        String date = formatter.format(calendar.getTime());
+        return date;
+    }
+
+    public static String today_4(){
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd", Locale.KOREA);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, +4);
+        String date = formatter.format(calendar.getTime());
+        return date;
+    }
+
+    public static String today_5(){
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd", Locale.KOREA);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, +5);
+        String date = formatter.format(calendar.getTime());
+        return date;
+    }
+}
--- app/src/main/java/kwaksuin/portfolio/weatherapp/MainActivity.java
+++ app/src/main/java/kwaksuin/portfolio/weatherapp/MainActivity.java
@@ -2,13 +2,51 @@ package kwaksuin.portfolio.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout location_layout;
+    ImageView location_img;
+    TextView location_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 지역선택 spinner 화면 이동
+        location_layout = findViewById(R.id.location_layout);
+        location_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Location.class);
+                startActivity(intent);
+            }
+        });
+
+        // 지역선택 spinner 화면 이동
+        location_img = findViewById(R.id.location_img);
+        location_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Location.class);
+                startActivity(intent);
+            }
+        });
+
+        // 지역선택 spinner 화면 이동
+        location_text = findViewById(R.id.location);
+        location_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Location.class);
+                startActivity(intent);
+            }
+        });
     }
 }
