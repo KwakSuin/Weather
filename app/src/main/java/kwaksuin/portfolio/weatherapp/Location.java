2f3de0e2fa89465475c90aaf003f7d91be1a4fc6
KwakSuin <mygirls0@naver.com>
2021-01-06 18:35:44 +0900

    Weather Application

--- app/src/main/java/kwaksuin/portfolio/weatherapp/Location.java
+++ app/src/main/java/kwaksuin/portfolio/weatherapp/Location.java
@@ -1,14 +1,126 @@
 package kwaksuin.portfolio.weatherapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Location extends AppCompatActivity {
+    ArrayAdapter<CharSequence> sido_adapter;
+    ArrayAdapter<CharSequence> sigungu_adapter;
+    Spinner sido;
+    Spinner sigungu;
+
+    String check_sido;
+    String check_sigungu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        sido = findViewById(R.id.sido_spinner);
+        sigungu = findViewById(R.id.sigungu_spinner);
+
+
+        // 스피너 뷰 어댑터 생성
+        sido_adapter = ArrayAdapter.createFromResource(this,R.array.spinner_sido, android.R.layout.simple_spinner_dropdown_item);
+        sido_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sido.setAdapter(sido_adapter);
+
+        sido.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // custom
+                ((TextView) parent.getChildAt(0)).setTextColor(Color.BLUE);
+                ((TextView) parent.getChildAt(0)).setTextSize(15);
+                parent.getChildAt(0).setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+
+                if (parent.getItemAtPosition(position).equals("선택해주세요")) {
+                    sigungu_adapter = ArrayAdapter.createFromResource(Location.this, R.array.spinner_string, android.R.layout.simple_spinner_item);
+                    sigungu_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    sigungu.setAdapter(sigungu_adapter);
+
+                } else if (parent.getItemAtPosition(position).equals("서울특별시")) {
+                    check_sido = "서울특별시";
+                    sigungu_adapter = ArrayAdapter.createFromResource(Location.this, R.array.spinner_seoul, android.R.layout.simple_spinner_item);
+                    sigungu_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    sigungu.setAdapter(sigungu_adapter);
+
+                    sigungu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            if (parent.getItemAtPosition(position).equals("선택해주세요")) {
+                                check_sigungu = null;
+                            } else {
+                                // 문자열 변환
+                                String item = parent.getItemAtPosition(position).toString();
+                                check_sigungu = item;
+                            }
+                        }
+                    });
+
+                } else if (parent.getItemAtPosition(position).equals("경기도")) {
+                    check_sido = "경기도";
+                    sigungu_adapter = ArrayAdapter.createFromResource(Location.this, R.array.spinner_gyeongi, android.R.layout.simple_spinner_item);
+                    sigungu_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    sigungu.setAdapter(sigungu_adapter);
+
+                } else {
+                    String item = parent.getItemAtPosition(position).toString();
+                }
+            }
+        });
+
+        sigungu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ((TextView) parent.getChildAt(0)).setTextColor(Color.BLUE);
+                ((TextView) parent.getChildAt(0)).setTextSize(15);
+                parent.getChildAt(0).setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        Button check_btn = findViewById(R.id.check_btn);
+        check_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(check_sido == "" || check_sigungu == "" || check_sido == null || check_sigungu == null){
+                    Toast.makeText(getApplicationContext(),"모두 선택해 주세요", Toast.LENGTH_SHORT).show();
+                } else {
+                    try{
+                        FileOutputStream outputStream = openFileOutput("file.text", Context.MODE_PRIVATE);
+                        String a = "";
+                        a = check_sido + " " + check_sigungu;
+                        outputStream.write(a.getBytes());
+                        outputStream.close();
+                        finish();
+
+                    } catch (IOException e){
+
+                    }
+                }
+            }
+        });
+
     }
 }
