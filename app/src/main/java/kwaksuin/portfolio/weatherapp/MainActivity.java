5c621ea55f299cd469e76084c35e304d93223130
KwakSuin <mygirls0@naver.com>
2021-01-22 16:18:36 +0900

    Weather Application

--- app/src/main/java/kwaksuin/portfolio/weatherapp/MainActivity.java
+++ app/src/main/java/kwaksuin/portfolio/weatherapp/MainActivity.java
@@ -12,11 +12,18 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+/*
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+ */
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -28,9 +35,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.lang.String;
-
-import jxl.Sheet;
-import jxl.Workbook;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -85,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
         private static final String TAG = "";
         String data;
         String sky;
+        String rain;
 
         String TM_min;
         String TM_max;
@@ -96,28 +101,50 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public void run() {
-            data = getXmlData();
-            sky = getXmlSky();
-            TM_min = getXmlTMN();
-            TM_max = getXmlTMX();
-            NTM_min = getXmlTMN_2();
-            NTM_max = getXmlTMX_2();
+            data = getXmlData();        // 현재 날씨
+            sky = getXmlSky();          // 현재 하늘
+            rain = getXmlRain();        // 현재 비
+
+
+            TM_min = getXmlTMN();       // 내일 최저온도
+            TM_max = getXmlTMX();       // 내일 최고온도
+
+            NTM_min = getXmlTMN_2();    // 모레 최저온도
+            NTM_max = getXmlTMX_2();    // 모레 최고온도
 
 
             Log.i(TAG,"현재 SKY : " + sky);
+            Log.i(TAG,"현재 RAIN : " + rain);
 
-            if(sky.equals("0") || sky.equals("1") || sky.equals("2") || sky.equals("3") || sky.equals("4") || sky.equals("5")){
-                // 맑음
-                runOnUiThread(() -> icon.setImageResource(R.drawable.sun_color));
+            // rain : 0 비안옴
+            if(rain.equals("0")){
+                if(sky.equals("0") || sky.equals("1") || sky.equals("2") || sky.equals("3") || sky.equals("4") || sky.equals("5")){
+                    // 맑음
+                    runOnUiThread(() -> icon.setImageResource(R.drawable.sun_color));
+                }
+                if(sky.equals("6") || sky.equals("7") || sky.equals("8")){
+                    // 구름많음
+                    runOnUiThread(() -> icon.setImageResource(R.drawable.cloud_color));
+                }
+                if(sky.equals("9") || sky.equals("10")) {
+                    // 흐림
+                    runOnUiThread(() -> icon.setImageResource(R.drawable.cloud_color));
+                }
+            }else{
+                if(rain.equals("1")){
+                    // 비
+                    runOnUiThread(() -> icon.setImageResource(R.drawable.rain_color));
+                }
+                if(rain.equals("2")){
+                    // 진눈개비
+                    runOnUiThread(() -> icon.setImageResource(R.drawable.rain_color));
+                }
+                if(rain.equals("3")){
+                    // 눈
+                    runOnUiThread(() -> icon.setImageResource(R.drawable.snow_color));
+                }
             }
-            if(sky.equals("6") || sky.equals("7") || sky.equals("8")){
-                // 구름많음
-                runOnUiThread(() -> icon.setImageResource(R.drawable.cloud_color));
-            }
-            if(sky.equals("9") || sky.equals("10")) {
-                // 흐림
-                runOnUiThread(() -> icon.setImageResource(R.drawable.cloud_color));
-            }
+
 
             Log.i(TAG,"현재 날씨 : " + data + " ℃");
             runOnUiThread(() -> today_temp.setText(data+ " ℃"));
@@ -253,6 +280,66 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return num.get(5);
+    }
+
+    // 초단기 실황, 비
+    // 비없음 : 0, 비 : 1, 진눈개비 : 2, 눈 : 3
+    public String getXmlRain(){
+        StringBuffer buffer = new StringBuffer();
+        String today = today();
+        String time = time();
+        String key = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtNcst?serviceKey=YXsM3Qh%2FJr8FVZdMZDqSOlosBCDFmdxqGACs6BCxXIfowyIig7ftX59UngDgR%2FVktpkhOJee84KB%2BbXrvaS1QA%3D%3D"
+                + "&base_date="
+                + today
+                + "&base_time="
+                + time
+                + "&numOfRows=150&nx=61&ny=130&_type=xml";
+
+        List<String> num = new ArrayList<>();
+
+        try{
+            URL url = new URL(key);
+            InputStream inputStream = url.openStream();
+
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = factory.newPullParser();
+            parser.setInput(new InputStreamReader(inputStream, "UTF-8"));
+
+            String tag;
+            parser.next();
+
+            int eventType = parser.getEventType();
+
+            while(eventType != XmlPullParser.END_DOCUMENT) {
+                switch (eventType){
+
+                    case XmlPullParser.START_TAG:
+                        tag = parser.getName();
+
+                        if(tag.equals("item"));
+
+                        else if(tag.equals("obsrValue")){
+                            parser.next();
+                            num.add(parser.getText());
+                        }
+                        break;
+
+                    case XmlPullParser.TEXT:
+                        break;
+
+                    case  XmlPullParser.END_TAG:
+                        tag = parser.getName();
+                        if (tag.equals("item")){
+                            buffer.append("");
+                        }
+                        break;
+                }
+                eventType = parser.next();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return num.get(0);
     }
 
     // 동네예보, 내일 최저기온
@@ -518,29 +605,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Excel 파일 불러오기
-    public void OpenExcel() {
-        try{
-            // 파일 읽어오기
-            InputStream inputStream = getBaseContext().getResources().getAssets().open("weather_xy.xlxs");
+    /*
+    public void OpenExcel() throws IOException {
+       FileInputStream file = new FileInputStream("C:\\Users\\Public\\Portfolio\\WeatherApp\\app\\src\\main\\assets\\weather_xy.xlsx");
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
+        int rowIndex = 0;
+        int columnIndex = 0;
 
-            // 엑셀 파일 정의
-            Workbook workbook = Workbook.getWorkbook(inputStream);
+        XSSFSheet sheet = workbook.getSheetAt(0);
 
-            if(workbook != null){
-                Sheet sheet = workbook.getSheet(0);     // 시트 불러오기
-                if(sheet != null){
-                    int column = sheet.getColumns();    // 전체 컬럼
-                    int rowIndex = 1;                   // row 인덱스 시작
-                    int rowTotal = sheet.getColumn(column-1).length;
+        int rows = sheet.getPhysicalNumberOfRows();
+        for(rowIndex = 1; rowIndex<rows; rowIndex++){
+            XSSFRow row = sheet.getRow(rowIndex);
+            if(row != null){
+                int cells = row.getPhysicalNumberOfCells();
+                for(columnIndex=0; columnIndex<=cells; columnIndex++){
+                    XSSFCell cell = row.getCell(columnIndex);
+
+                    String value = "";
+                    if(cell == null){
+                        continue;
+                    }else{
+                        switch (cell.getCellType()){
+                            case FORMULA:
+                                value = cell.getCellFormula();
+                                break;
+
+                            case NUMERIC:
+                                value = cell.getNumericCellValue()+"";
+                                break;
+
+                            case STRING:
+                                value = cell.getStringCellValue()+"";
+                                break;
+
+                            case BLANK:
+                                value = cell.getBooleanCellValue()+"";
+                                break;
+
+                            case ERROR:
+                                value = cell.getErrorCellValue()+"";
+                                break;
+
+                        }
+                    }
                 }
             }
-        }catch (Exception e){
-            e.printStackTrace();
         }
-
-
-
     }
+
+     */
 
     // spinner 지역 선택 후, 현재 지역으로 setText
     @Override
